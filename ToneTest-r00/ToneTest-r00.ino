3d3888ea3f85ea768ccb9d9305d63bc7a1647fcc
SaLeeC <20824335+SaLeeC@users.noreply.github.com>
2019-10-21 22:13:05 +0200

    Add files via upload

--- ToneTest-r00/ToneTest-r00.ino
+++ ToneTest-r00/ToneTest-r00.ino
@@ -1,3 +1,15 @@
+// Test del comado tone()
+//Il codice esegue un too di frequenza Tono1 e durata TTono1
+//sul piedino Sirea (pin 8 dove la Maker Uno ha collegato il buzzer)
+
+//Il programma ci forisce la lettura del timer intero della scheda
+//espresso in millisecondi rispettivamente:
+// quando inizia il prgramma
+// ogni volta che fa il ciclo della loop()
+
+//La velocità di comunicazione della seriale è VelocitaSeriale
+
+
 #define VelocitaSeriale 9600
 
 #define Sirena 8
